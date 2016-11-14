5f662c58352e6a787b73f3957c33bc49b954d2b3
Erik Noorland <mail@eriknoorland.nl>
2016-11-14 19:56:58 +0100

    Update rotary encoder implementation, Implement select, store and exit push buttons, Set correct pin numbers, Update version number

--- arduinoMidiAmpSwitcher.ino
+++ arduinoMidiAmpSwitcher.ino
@@ -1,41 +1,48 @@
 #include <MIDI.h>
 
 // software version number (major.minor.hotfix)
-const String VERSION_NUMBER = "0.1.0";
+const String VERSION_NUMBER = "0.2.0";
 
 // default midi channel is used when the user
 // has not yet stored a midi channel
-const int DEFAULT_MIDI_CHANNEL = 1;
+const byte DEFAULT_MIDI_CHANNEL = 1;
 
 // The most left digit stands for the first output
 // and the most right digit for the last output
-const int numProgramPresets = 17;
-const String programPresets[17] = {
+const byte numProgramPresets = 17;
+const String programPresets[numProgramPresets] = {
   "----", "0000", "0001", "0010", "0011", "0100", "0101", "0110", "0111",
   "1000", "1001", "1010", "1011", "1100", "1101", "1110", "1111"
 };
 
 // output pins
 const byte activityPin = 13;
-const byte outputPin1 = 13;
-const byte outputPin2 = 13;
-const byte outputPin3 = 13;
-const byte outputPin4 = 13;
+const byte outputPin1 = 9;
+const byte outputPin2 = 10;
+const byte outputPin3 = 11;
+const byte outputPin4 = 12;
 
-int numOutputPins = 5;
-int outputPins[5] = {
+const byte numOutputPins = 5;
+const byte outputPins[numOutputPins] = {
   activityPin, outputPin1, outputPin2, outputPin3, outputPin4
 };
 
 // input pins
-const byte rotaryPinA = 3;
+const byte rotaryPinA = 2;
 const byte rotaryPinB = 3;
-const byte selectButtonPin = 3;
-const byte storeButtonPin = 3;
-const byte exitButtonPin = 3;
+const byte selectButtonPin = 4;
+const byte storeButtonPin = 5;
+const byte exitButtonPin = 6;
 
-int numInputPins = 3;
-int inputPins[3] = {
+boolean selectButtonState = LOW;
+boolean selectButtonLastState = LOW;
+boolean storeButtonState = LOW;
+boolean storeButtonLastState = LOW;
+boolean exitButtonState = LOW;
+boolean exitButtonLastState = LOW;
+
+const byte numInputPins = 5;
+const byte inputPins[numInputPins] = {
   rotaryPinA,
   rotaryPinB,
   selectButtonPin,
@@ -43,7 +50,13 @@ int inputPins[3] = {
   exitButtonPin
 };
 
-int midiChannel = DEFAULT_MIDI_CHANNEL;
+const int rotaryBump[] = {0, -1, 0, 1};
+byte rotaryValue = 0;
+byte minRotaryValue = 0;
+byte maxRotaryValue = 10; // number of array length of the current scrolling list
+boolean loopRotaryValue = true; // get to the beginning of the array when at the end and vice versa
+
+byte midiChannel = DEFAULT_MIDI_CHANNEL;
 
 MIDI_CREATE_DEFAULT_INSTANCE();
 
@@ -57,9 +70,19 @@ void setup() {
   }
 
   // setup input pin modes
+  // setup inputs using the internal pullup resistors
   for(int j = 0; j < numInputPins; j++) {
     pinMode(inputPins[j], INPUT);
+    digitalWrite(inputPins[j], HIGH);
   }
+
+  // setup button states
+  selectButtonState = digitalRead(selectButtonPin);
+  selectButtonLastState = selectButtonState;
+  storeButtonState = digitalRead(storeButtonPin);
+  storeButtonLastState = storeButtonState;
+  exitButtonState = digitalRead(exitButtonPin);
+  exitButtonLastState = exitButtonState;
 
   // get the user stored channel or use the default
   // midiChannel = <EEPROM data>;
@@ -68,6 +91,11 @@ void setup() {
   MIDI.begin(midiChannel);
   MIDI.setHandleControlChange(onControlChange);
   MIDI.setHandleProgramChange(onProgramChange);
+
+  // setup rotary listener
+  attachInterrupt(0, onRotaryUpdate, RISING);
+
+  Serial.begin(9600);
 }
 
 /**
@@ -75,6 +103,29 @@ void setup() {
  */
 void loop() {
   MIDI.read();
+
+  selectButtonState = digitalRead(selectButtonPin);
+  storeButtonState = digitalRead(storeButtonPin);
+  exitButtonState = digitalRead(exitButtonPin);
+
+  if(selectButtonState == HIGH && selectButtonLastState == LOW) {
+   onSelectButtonPress();
+  }
+  delay(1);
+
+  if(storeButtonState == LOW && storeButtonLastState == HIGH) {
+   onStoreButtonPress();
+  }
+  delay(1);
+
+  if(exitButtonState == LOW && exitButtonLastState == HIGH) {
+   onExitButtonPress();
+  }
+  delay(1);
+
+  selectButtonLastState = selectButtonState;
+  storeButtonLastState = storeButtonState;
+  exitButtonLastState = exitButtonState;
 }
 
 /**
@@ -121,4 +172,48 @@ void onProgramChange(byte channel, byte number) {
   // loop through the preset and set the outputs accordingly
 
   onMidiActivity();
+}
+
+/**
+ * Rotary change handler
+ */
+void onRotaryUpdate() {
+  byte state = 0;
+
+  state += digitalRead(rotaryPinA);
+  state <<= 1;
+  state += digitalRead(rotaryPinB);
+
+  rotaryValue += rotaryBump[state];
+
+  if(rotaryValue == maxRotaryValue) {
+    rotaryValue = loop ? 0 : maxRotaryValue - 1;
+  }
+
+  if(rotaryValue < minRotaryValue) {
+    rotaryValue = loop ? maxRotaryValue - 1 : 0;
+  }
+
+  Serial.println(rotaryValue);
+}
+
+/**
+ * Select button press handler
+ */
+void onSelectButtonPress() {
+  Serial.println("Select button pressed!");
+}
+
+/**
+ * Store button press handler
+ */
+void onStoreButtonPress() {
+  Serial.println("Store button pressed!");
+}
+
+/**
+ * Exit button press handler
+ */
+void onExitButtonPress() {
+  Serial.println("Exit button pressed!");
 }
